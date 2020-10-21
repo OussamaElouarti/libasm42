449d262cded80676ea71af85b5f7ad19c9a9ad73
OussamaElouarti <49252717+OussamaElouarti@users.noreply.github.com>
2020-10-21 16:44:40 +0100

    Add files via upload
    erno in read and write

--- libasm/ft_read.s
+++ libasm/ft_read.s
@@ -1,6 +1,16 @@
 section .text
     global _ft_read
+    extern ___error
 _ft_read:
     mov rax, 0x2000003
     syscall
+    jc _error
+    ret
+_error:
+    push rbx
+    mov rax, rbx
+    call ___error
+    mov [rax], rbx
+    pop rbx
+    mov rax, -1
     ret
--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -1,6 +1,17 @@
 section .text
     global _ft_write
+    extern ___error
 _ft_write:
     mov rax, 0x2000004
     syscall
+    jc _error
     ret
+_error:
+    push rbx
+    mov rax, rbx
+    call ___error
+    mov [rax], rbx
+    pop rbx
+    mov rax, -1
+    ret
+    
